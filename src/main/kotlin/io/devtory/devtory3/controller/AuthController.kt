d8b5b4aa39685dc6521cf2c58304fc9570704b4e
byeongdoohan <quden04@gmail.com>
2026-02-03 23:37:30 +0900

    feat: login, logout, refresh 추가

--- src/main/kotlin/io/devtory/devtory3/controller/AuthController.kt
+++ src/main/kotlin/io/devtory/devtory3/controller/AuthController.kt
@@ -2,17 +2,24 @@ package io.devtory.devtory3.controller
 
 import io.devtory.devtory3.dto.*
 import io.devtory.devtory3.service.AuthService
+import io.devtory.devtory3.util.UserDetails
 import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseCookie
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 class AuthController(
-    private val authService: AuthService
+    private val authService: AuthService,
+    @Value("\${jwt.refresh-token-expiration}") private val REFRESH_TOKEN_EXPIRATION: Long
 ) {
 
     /**
@@ -29,16 +36,21 @@ class AuthController(
      */
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    suspend fun login(@Valid @RequestBody request: LoginRequest, response: HttpServletResponse): TokenResponse {
+    suspend fun login(@Valid @RequestBody request: LoginRequest, response: HttpServletResponse): ResponseEntity<TokenResponse> {
         val tokenResponse = authService.login(request)
-        val cookie = Cookie("refreshToken", tokenResponse.refreshToken).apply {
-            isHttpOnly = true
-            secure = true
-            path = "/"
-            maxAge = 7 * 24 * 60 * 60 // 7 days
-        }
-        response.addCookie(cookie)
-        return authService.login(request)
+        val refreshCookie = ResponseCookie.from("refreshToken", tokenResponse.refreshToken)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(REFRESH_TOKEN_EXPIRATION) // 7 days
+            .sameSite("Strict")
+            .build()
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+            .body(TokenResponse(
+                accessToken = tokenResponse.accessToken,
+                refreshToken = null
+            ))
     }
 
     /**
@@ -46,15 +58,37 @@ class AuthController(
      */
     @PostMapping("/refresh")
     @ResponseStatus(HttpStatus.OK)
-    suspend fun refresh(@AuthenticationPrincipal @Valid @RequestBody request: RefreshTokenRequest, response: HttpServletResponse): TokenResponse {
-        val tokenResponse = authService.refreshToken(request)
-        val cookie = Cookie("refreshToken", tokenResponse.refreshToken).apply {
-            isHttpOnly = true
-            secure = true
-            path = "/"
-            maxAge = 7 * 24 * 60 * 60 // 7 days
-        }
-        response.addCookie(cookie)
-        return tokenResponse
+    suspend fun refresh(request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<TokenResponse> {
+        val tokenResponse = authService.refresh(request)
+
+        val refreshCookie = ResponseCookie.from("refreshToken", tokenResponse.refreshToken)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(7 * 24 * 60 * 60) // 7 days
+            .sameSite("Strict")
+            .build()
+        response.addHeader("refreshToken", tokenResponse.refreshToken)
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+            .body(TokenResponse(
+                accessToken = tokenResponse.accessToken,
+                refreshToken = null,
+            ))
+    }
+
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    suspend fun logout(request: HttpServletRequest, response:HttpServletResponse):ResponseEntity<Void> {
+        authService.logout(request)
+        val deleteCookie = ResponseCookie.from("refreshToken", "")
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(7 * 24 * 60 * 60) // 7 days
+            .sameSite("Strict")
+            .build()
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, deleteCookie.toString()).build()
     }
 }
--- src/main/kotlin/io/devtory/devtory3/service/AuthService.kt
+++ src/main/kotlin/io/devtory/devtory3/service/AuthService.kt
@@ -3,24 +3,28 @@ package io.devtory.devtory3.service
 import io.devtory.devtory3.domain.User
 import io.devtory.devtory3.dto.*
 import io.devtory.devtory3.entity.UserEntity
-import io.devtory.devtory3.exception.DuplicateEmailException
-import io.devtory.devtory3.exception.DuplicateNicknameException
-import io.devtory.devtory3.exception.UserNotFoundException
+import io.devtory.devtory3.exception.*
 import io.devtory.devtory3.repository.UserRepository
 import io.devtory.devtory3.security.JwtTokenProvider
-import io.devtory.devtory3.security.PasswordEncoder
+import jakarta.servlet.http.HttpServletRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
 class AuthService(
     private val userRepository: UserRepository,
     private val redisService: RedisService,
+    private val passwordEncoder: PasswordEncoder,
     private val jwtTokenProvider: JwtTokenProvider
 ) {
-    private val logger = LoggerFactory.getLogger(javaClass)
+    private val LOGGER = LoggerFactory.getLogger(javaClass)
 
     /**
      * 회원가입
@@ -31,14 +35,14 @@ class AuthService(
 
         // 중복 확인
         if (userRepository.existsByEmail(email)) {
-            throw DuplicateEmailException(request.email)
+            throw CustomException(ErrorCode.USER_ALREADY_EXISTED)
         }
         if (userRepository.existsByNickname(nickname)) {
-            throw DuplicateNicknameException(request.nickname)
+            throw CustomException(ErrorCode.NICKNAME_ALREADY_EXISTED)
         }
 
         // 비밀번호 해싱
-        val passwordHash = PasswordEncoder.encode(request.password)
+        val passwordHash = passwordEncoder.encode(request.password)
 
         // 사용자 생성
         val user = User.create(
@@ -64,38 +68,100 @@ class AuthService(
         )
     }
 
+    @Transactional
     suspend fun login(request: LoginRequest): TokenResponse {
+        // 사용자 조회
         val userEntity = withContext(Dispatchers.IO) {
             userRepository.findByEmail(request.email)
-        } ?:throw UserNotFoundException(request.email)
+        } ?:throw CustomException(ErrorCode.USER_NOT_FOUND)
 
-        val accessToken = jwtTokenProvider.createAccessToken(userEntity.id, userEntity.email, userEntity.role.name)
-        val refreshToken = jwtTokenProvider.createRefreshToken(userEntity.id, userEntity.email, userEntity.role.name)
+        // 비밀번호 검증
+        if(!passwordEncoder.matches(request.password, userEntity.passwordHash)){
+            throw CustomException(ErrorCode.PASSWORD_MISMATCH)
+        }
 
-        redisService.saveRefreshToken(userEntity.id, refreshToken)
+        // 인증정보 생성
+        val role = ROLE + userEntity.role
+        val authentication = UsernamePasswordAuthenticationToken(
+            userEntity.email,
+            null,
+            listOf(SimpleGrantedAuthority(role))
+        )
 
+        // 인증정보 통한 토큰 발급
+        val accessToken = jwtTokenProvider.createAccessToken(authentication)
+        val refreshToken = jwtTokenProvider.createRefreshToken(authentication)
+
+        // redis에 refresh token 저장
+        redisService.saveRefreshToken(userEntity.email, refreshToken)
+
+        // 토큰 반환
         return TokenResponse(
             accessToken = accessToken,
-            refreshToken = refreshToken,
-            userId = userEntity.id
+            refreshToken = refreshToken
         )
     }
 
-    suspend fun refreshToken(request: RefreshTokenRequest): TokenResponse {
-        redisService.deleteRefreshToken(request.refreshToken)
-        val userId = jwtTokenProvider.getUserIdFromToken(request.refreshToken)
-        val email = jwtTokenProvider.getEmailFromToken(request.refreshToken)
-        val role = jwtTokenProvider.getRoleFromToken(request.refreshToken)
+    @Transactional
+    suspend fun logout(request: HttpServletRequest) {
+        val authHeader: String? = request.getHeader(AUTHORIZATION_HEADER)
+        if (authHeader == null || !authHeader.startsWith(GRANT_TYPE)) {
+            throw CustomException(ErrorCode.INVALID_TOKEN)
+        }
+        val token = authHeader.substring(7)
+        val email = jwtTokenProvider.getAuthentication(token)?.name.orEmpty()
+        redisService.deleteRefreshToken(email)
+    }
 
-        val accessToken = jwtTokenProvider.createAccessToken(userId,email,role)
-        val refreshToken = jwtTokenProvider.createRefreshToken(userId,email,role)
+    @Transactional
+    suspend fun refresh(request: HttpServletRequest): TokenResponse {
+        val authHeader: String? = request.getHeader(AUTHORIZATION_HEADER)
+        if (authHeader == null || !authHeader.startsWith(GRANT_TYPE)) {
+            throw CustomException(ErrorCode.INVALID_TOKEN)
+        }
 
-        redisService.saveRefreshToken(userId, refreshToken)
+        val refreshToken = authHeader.substring(7)
+        if(jwtTokenProvider.getTokenType(refreshToken)!= REFRESH){
+            throw CustomException(ErrorCode.INVALID_TOKEN)
+        }
 
+        val email = jwtTokenProvider.getAuthentication(refreshToken)?.name.orEmpty()
+
+        // redis에 저장된 refresh token과 비교
+        if(!redisService.getRefreshToken(email).equals(refreshToken)){
+            throw CustomException(ErrorCode.EXPIRED_TOKEN)
+        }
+
+        // 기존 refresh token 제거
+        redisService.deleteRefreshToken(email)
+
+        // 새로운 인증정보
+        val role = ROLE_USER
+        val authentication = UsernamePasswordAuthenticationToken(
+            email,
+            null,
+            listOf(SimpleGrantedAuthority(role))
+        )
+
+        // 인증정보 통한 토큰 발급
+        val accessToken = jwtTokenProvider.createAccessToken(authentication)
+        val newRefreshToken = jwtTokenProvider.createRefreshToken(authentication)
+
+        // redis에 refresh token 저장
+        redisService.saveRefreshToken(email, newRefreshToken)
+
+        // 토큰 반환
         return TokenResponse(
             accessToken = accessToken,
-            refreshToken = refreshToken,
-            userId = userId
+            refreshToken = newRefreshToken
         )
+    }
+
+    companion object{
+        const val AUTHORIZATION_HEADER = "Authorization"
+        private const val GRANT_TYPE = "Bearer "
+        private const val ROLE_USER = "ROLE_USER"
+        private const val ROLE = "ROLE_"
+        private const val REFRESH = "refresh"
     }
 }

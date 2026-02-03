85a7efa0a527c2dfe8145fd0bb628237af46825f
byeongdoohan <quden04@gmail.com>
2026-02-03 22:22:53 +0900

    feat: error code 추가

--- src/main/kotlin/io/devtory/devtory3/exception/ErrorCode.kt
+++ src/main/kotlin/io/devtory/devtory3/exception/ErrorCode.kt
@@ -9,7 +9,13 @@ enum class ErrorCode(httpStatus: HttpStatus, errorCode: String, errormessage: St
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-002", "Expired token."),
     UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-003", "Unsupported token."),
     WRONG_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-004", "Wrong token."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "AUTH-005", "Access denied.");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "AUTH-005", "Access denied."),
+    PASSWORD_MISMATCH(HttpStatus.UNAUTHORIZED, "AUTH-006", "Password mismatch."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH-007", "User not found."),
+
+    USER_ALREADY_EXISTED(HttpStatus.CONFLICT, "AUTH-008", "User already existed."),
+    NICKNAME_ALREADY_EXISTED(HttpStatus.CONFLICT, "AUTH-009", "Nickname already existed."),
+    ;
 
     val status: HttpStatus = httpStatus
     val code: String = errorCode

afdddd4fac655dc8224d8d349aa49b9d48dc17ca
byeongdoohan <quden04@gmail.com>
2026-02-09 21:41:41 +0900

    feat: story cursor 조회 추가

--- src/main/kotlin/io/devtory/devtory3/repository/StoryRepository.kt
+++ src/main/kotlin/io/devtory/devtory3/repository/StoryRepository.kt
@@ -2,8 +2,18 @@ package io.devtory.devtory3.repository
 
 import io.devtory.devtory3.entity.StoryEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface StoryRepository : JpaRepository<StoryEntity, Long> {
+    @Query(
+        """
+        SELECT s FROM StoryEntity s
+        JOIN FETCH s.author a
+        WHERE (:cursor IS NULL OR s.id < :cursor)
+        ORDER BY s.id DESC
+        """
+    )
+    fun findNextPage(cursor: Long?, size: Int): List<StoryEntity>
 }
